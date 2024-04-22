7ce14d54154eaa238e3a60bde30374ad653fd5e6
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-04-22 19:05:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    credentials = "terraform-proj-service-account.json"
+    #credentials = "terraform-proj-service-account.json"
     bucket      = "terraform-pro-tf-backend"
     prefix      = "env/terraform-test"
   }
