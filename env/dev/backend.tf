1ca99ae9b6826340721926d1a1ba4073f8e8b928
begh-azka <azkabegh59@gmail.com>
2024-04-24 11:21:57 +0530

    chnged

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     #credentials = "terraform-proj-service-account.json"
-    bucket      = "terraform-pro-tf-backend"
-    prefix      = "env/terraform-test"
+    bucket      = "tf-dev-log-bucket"
+    prefix      = "env/terraform-dev"
   }
 }
