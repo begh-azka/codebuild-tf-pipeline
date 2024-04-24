c9ac67066a758fcfc02d92e954a3f62c0db9e31b
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-04-24 10:17:03 +0530

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,6 +1,5 @@
 // Configure the Google Cloud provider
 provider "google" {
-  #credentials = file(var.credentials_file)
   project     = var.project
   region      = var.region
   zone        = var.zone
